6f550da3d11b7bb1f41ddc0faddd65cd1e20ece0
nitiphoom.y <nitiphoom.y@ku.th>
2024-09-24 17:00:48 +0700

    [FIX] config cypress :bug:

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,11 +6,17 @@ export default defineConfig({
     baseUrl: 'http://localhost:3000', // Set the base URL for your app
     specPattern: 'cypress/e2e/**/*.cy.ts', // Location of test files
     supportFile: 'cypress/support/e2e.ts', // Support file (if any)
+    
     setupNodeEvents(on, config) {
       reportingSetup(on); // Use the reporting setup
+      console.log('Cypress configuration:', config); // Log config for troubleshooting
     },
   },
-  video: false, // Disable video recording
-  screenshotsFolder: 'cypress/screenshots', // Customize folder for screenshots
-  videosFolder: 'cypress/videos', // Customize folder for videos
+  
+  video: true,  // Enable video recording
+  screenshotsFolder: 'cypress/screenshots',
+  videosFolder: 'cypress/videos',
+  videoCompression: 32,  // Compress video to save space
+  videoUploadOnPasses: false,  // Prevent video upload for passing tests
+  screenshotOnRunFailure: true,  // Take screenshot on test failure
 });
--- cypress/e2e/home/spec.cy.ts
+++ cypress/e2e/home/spec.cy.ts
@@ -1,11 +1,26 @@
 describe('Text Presence Test', () => {
-  it('should check if the page contains specific text', () => {
+  it('should check if the page contains specific text and interact with elements', () => {
     // Visit your application's URL
     cy.visit('/home'); // Change this to the relevant path or baseUrl
 
-    // Check if the element with ID 't-01' contains the text 'Welcome'
-    cy.get('#t-home').should('contain', 'Home');
+    // Check if the element with ID 't-01' contains the text 'My Home Page'
+    cy.get('#t-01').should('contain', 'My Home Page');
+    cy.screenshot('test-screenshot');
+
+    // Add text 'nitiphoom' into the input field with ID 'in-name'
+    cy.get('#in-name').type('nitiphoom');
+    cy.screenshot('test-screenshot');
+
+
+    // Check if the 'Content' button is visible
+    cy.get('#bt-to-content').should('be.visible');
+
+    // Click the 'Content' button
+    cy.get('#bt-to-content').click();
+
+    // After clicking, you may want to verify navigation to the '/content' page
+    cy.url().should('include', '/content');
+    cy.screenshot('test-screenshot');
+
   });
 });
-
-
